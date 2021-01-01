6b62563a942094c22590494a4ea426a80f54e80d
thesanjeetc <21122782+thesanjeetc@users.noreply.github.com>
2021-01-01 21:46:07 +0000

    Updated Payload Limit

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, "App/build")));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({limit: '50mb',extended: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
