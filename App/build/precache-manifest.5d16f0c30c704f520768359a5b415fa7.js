21bf951ca4c09608d9221e3dfbdd37871d137cf4
thesanjeetc <21122782+thesanjeetc@users.noreply.github.com>
2021-01-01 21:07:15 +0000

    Updated Build Process

---
+++ App/build/precache-manifest.5d16f0c30c704f520768359a5b415fa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1a47864d2349cdc4d545e14601ccfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d44cfc61305d711b2e8",
+    "url": "/static/css/main.591963bc.chunk.css"
+  },
+  {
+    "revision": "e7e92ddef3b4c9e6fe40",
+    "url": "/static/js/2.727fa100.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.727fa100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d44cfc61305d711b2e8",
+    "url": "/static/js/main.559c3bcf.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
